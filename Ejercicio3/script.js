4269cf9ef8bf21c8991db981913d4c1d3ce91b1f
angelarc20 <66035152+angelarc20@users.noreply.github.com>
2021-02-23 21:15:31 -0600

    Ejercicio3

---
+++ Ejercicio3/script.js
@@ -0,0 +1,29 @@
+function AgregarFila(){
+    var fecha = document.getElementById("fecha").value; 
+    var bebida = document.getElementById("bebida").value; 
+    var cantidad = document.getElementById("cantidad").value; 
+
+    var c1 = document.createElement("c"); 
+    var c2 = document.createElement("c"); 
+    var c3 = document.createElement("c");
+    var c4 = document.createElement("c");
+    var f = document.createElement("f"); 
+
+   
+    var message; 
+    if (cantidad < 8 ) {
+        message = "tomar mas agua";
+    } else {
+        message = "meta alcanzada";
+    }
+
+ 
+        f.appendChild(c1);
+        f.appendChild(c2);
+        f.appendChild(c3);
+        f.appendChild(c4);
+        
+
+        document.getElementById("tabla1").appendChild(f);
+    }
+
